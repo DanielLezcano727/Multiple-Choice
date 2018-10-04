7007c088a5d6f4a8dbaf38fc98d215b898302acd
Micaela Siles <micaela.b.siles@gmail.com>
2018-10-04 11:24:22 -0300

    Agregado el algoritmo para mezclar las preguntas y limitar su cantidad y agregados sus respectivos tests

--- src/MultipleChoice.php
+++ src/MultipleChoice.php
@@ -5,7 +5,41 @@ namespace MultiChoice;
 use Symfony\Component\Yaml\Yaml;
 
 class MultipleChoice{
-    public function __construct(){
-        $this->mp = 1;
+    protected $cantPreguntas;
+    protected $preguntas;
+    public function __construct($cantPreguntas = 12){
+
+        $this->preguntas = Yaml::parseFile('Preguntas/preguntas.yml');
+
+        if($cantPreguntas < count($this->preguntas['preguntas']) && $cantPreguntas > 0){
+            $this->cantPreguntas = $cantPreguntas;
+        }else{
+            $this->cantPreguntas = 12;            
+        }
+
+
+
+    }
+
+    public function organizar(){
+        $this->preguntas = $this->mezclar($this->preguntas['preguntas'],$this->cantPreguntas);
+    }
+
+
+    public function mezclar($array, $cant){
+        shuffle($array);
+        for($i = count($array);$i>$cant;$i--){
+            array_pop($array);
+        }
+
+        return $array;
+    }
+
+    public function devolverPreguntas(){
+        return $this->preguntas['preguntas'];
+    }
+
+    public function devolverCantidad(){
+        return $this->cantPreguntas;
     }
 }
--- tests/MultipleChoiceTest.php
+++ tests/MultipleChoiceTest.php
@@ -3,6 +3,7 @@
 namespace MultiChoice;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
 
 class MultipleChoiceTest extends TestCase{
 
@@ -11,4 +12,48 @@ class MultipleChoiceTest extends TestCase{
         $this->assertTrue(isset($MultChoice));
     }
 
+    public function testMezclar(){
+        $mult = new MultipleChoice();
+        $preguntas = Yaml::parseFile('Preguntas/preguntas.yml');
+        for($i = count($preguntas['preguntas']);$i>12;$i--){
+            array_pop($preguntas['preguntas']);
+        }
+        $preguntasMezcladas1 = $mult->mezclar($mult->devolverPreguntas(),$mult->devolverCantidad());
+        $this->assertNotEquals($preguntas, $preguntasMezcladas1);
+        $this->assertEquals(12, count($preguntasMezcladas1) );
+
+        $mult = new MultipleChoice();
+        $preguntasMezcladas2 = $mult->mezclar($mult->devolverPreguntas(),$mult->devolverCantidad());
+        $this->assertNotEquals($preguntas, $preguntasMezcladas2);
+        $this->assertEquals(12, count($preguntasMezcladas2) );
+
+        $mult = new MultipleChoice();
+        $preguntasMezcladas3 = $mult->mezclar($mult->devolverPreguntas(),$mult->devolverCantidad());
+        $this->assertNotEquals($preguntas, $preguntasMezcladas3);
+        $this->assertEquals(12, count($preguntasMezcladas3) );
+
+        $this->assertNotEquals($preguntasMezcladas2,$preguntasMezcladas1);
+        $this->assertNotEquals($preguntasMezcladas2,$preguntasMezcladas3);
+
+    }
+
+    public function testCantidadPreguntas(){
+        $mult = new MultipleChoice(15);
+        $this->assertEquals(15, $mult->devolverCantidad());
+        $mult = new MultipleChoice(1);
+        $this->assertEquals(1, $mult->devolverCantidad());
+        $mult = new MultipleChoice(22);
+        $this->assertEquals(22, $mult->devolverCantidad());
+        $mult = new MultipleChoice(88);
+        $this->assertEquals(12, $mult->devolverCantidad());
+        $mult = new MultipleChoice(-88);
+        $this->assertEquals(12, $mult->devolverCantidad());
+    }
+
+    public function testPreguntas(){
+        $preguntas = Yaml::parseFile('Preguntas/preguntas.yml');
+        $preguntas = $preguntas['preguntas'];
+        $mult = new MultipleChoice();
+        $this->assertEquals($preguntas,$mult->devolverPreguntas());        
+    }
 }
