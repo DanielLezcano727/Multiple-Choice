d76200af8ada6ac1b088cd4264f0ae008412dc0a
Daniel Lezcano <daniel.lezcano727@gmail.com>
2018-10-07 22:42:29 -0300

    Agregada la transformacion de las respuestas para reconocer si son verdaderas o falsas

--- src/MultipleChoice.php
+++ src/MultipleChoice.php
@@ -7,6 +7,7 @@ use Symfony\Component\Yaml\Yaml;
 class MultipleChoice{
     protected $cantPreguntas;
     protected $preguntas;
+    
     public function __construct($cantPreguntas = 12){
 
         $this->preguntas = Yaml::parseFile('Preguntas/preguntas.yml');
@@ -23,6 +24,10 @@ class MultipleChoice{
 
     public function organizar(){
         $this->preguntas = $this->mezclar($this->preguntas['preguntas'],$this->cantPreguntas);
+        for($i=0;$i<$this->cantPreguntas;$i++){
+            $this->preguntas[$i] = $this->inicializarRespuestas($this->preguntas[$i]);
+            print_r($this->devolverRespuestas($this->preguntas[$i]));
+        }
     }
 
 
@@ -54,5 +59,29 @@ class MultipleChoice{
             array_push($aux,$pregunta['respuestas_correctas'][$i]);
         }
         return $aux;
+    }
+
+    public function inicializarRespuestas($pregunta){
+        $cant = count($pregunta['respuestas_incorrectas']);
+
+        if($cant == 0){
+            $pregunta['respuestas_incorrectas'] = '1Todas las anteriores';
+        }
+
+        for($i=0;$i<$cant;$i++){
+            $pregunta['respuestas_incorrectas'][$i] = "0" . $pregunta['respuestas_incorrectas'][$i];
+        }
+
+        $cant = count($pregunta['respuestas_correctas']);
+
+        if($cant == 0){
+            $pregunta['respuestas_incorrectas'] = '1Ninguna de las anteriores';
+        }
+
+        for($i=0;$i<$cant;$i++){
+            $pregunta['respuestas_correctas'][$i] = "1" . $pregunta['respuestas_correctas'][$i];
+        }
+        
+        return $pregunta;
     }
 }
